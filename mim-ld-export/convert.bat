d566a22c5f9d8358fc8633c4179b1e6326ffb985
Jesse Bakker <jesse.bakker@kadaster.nl>
2023-05-10 12:06:56 +0200

    Update ontology generation workflow

--- mim-ld-export/convert.bat
+++ mim-ld-export/convert.bat
@@ -1,32 +1,21 @@
-REM  Conversion of MIM UML to MIM-LD
-REM  Used for local conversion
-REM  Use pipeline version for automatic conversion in github
-REM  Dependencies:
-REM  - https://github.com/architolk/ea2rdf
-REM  - https://github.com/architolk/rdf2rdf
-REM  - https://github.com/architolk/mimtools
-REM 
-
-REM Debug
-REM java -jar libs/ea2rdf.jar -e "../nen3610-2022-template.eap" > nen3610-2022-db.ttl
 
 REM  First step: transform native EAP format to RDF
-java -jar libs/ea2rdf.jar -ea -0 -e "../ea/imx-geo.eapx" > imx-geo-ea.ttl
+java -jar mim-ld-export/libs/ea2rdf.jar -e "ea/imx-geo.eapx" > mim-ld-export/temp/imx-geo-ea.ttl
 
 REM  Second step: transform EA model in RDF to MIM in RDF
-java -jar libs/rdf2rdf.jar imx-geo-ea.ttl imx-geo-mim-all.ttl libs/ea2mim.yaml
+java -jar mim-ld-export/libs/rdf2rdf.jar mim-ld-export/temp/imx-geo-ea.ttl mim-ld-export/temp/imx-geo-mim-all.ttl mim-ld-export/libs/wau-ea2mim.yaml
 
 REM  Third step: get only the Informatiemodel of interest from the EA model
-java -jar libs/rdf2rdf.jar imx-geo-mim-all.ttl imx-geo-mim.ttl imx-geo-split.yaml
+java -jar mim-ld-export/libs/rdf2rdf.jar mim-ld-export/temp/imx-geo-mim-all.ttl mim-ld-export/model/imx-geo-mim.ttl mim-ld-export/libs/imx-geo-split.yaml
 
 REM  Fourth step: transform MIM model in RDF to RDFS/OWL/SHACL ontology
-java -jar libs/rdf2rdf.jar imx-geo-mim.ttl imx-geo-ont.ttl libs/mim2onto.yaml
+java -jar mim-ld-export/libs/rdf2rdf.jar mim-ld-export/model/imx-geo-mim.ttl mim-ld-export/model/imx-geo-ont.ttl mim-ld-export/libs/wau-mim2onto.yaml
 
-REM  Fifth step: specific IMX-Geo steps. Not yet clear if needed
-REM java -jar libs/rdf2rdf.jar imx-geo-ont.ttl ../imx-geo-ontologie.ttl imx-geo.yaml imx-geo-mim.ttl
+@REM REM  Fifth step: specific IMX-Geo steps. Not yet clear if needed
+@REM REM java -jar libs/rdf2rdf.jar imx-geo-ont.ttl ../imx-geo-ontologie.ttl imx-geo.yaml imx-geo-mim.ttl
 
-REM  Sixth step: create diagram from ontology
-REM java -jar libs/rdf2xml.jar ../imx-geo-ontologie.ttl imx-geo-model.graphml libs/rdf2graphml.xsl
+@REM REM  Sixth step: create diagram from ontology
+@REM REM java -jar libs/rdf2xml.jar ../imx-geo-ontologie.ttl imx-geo-model.graphml libs/rdf2graphml.xsl
 
-REM  Seventh step: create MD file for respec page (werkt nog niet)
-REM java -jar libs/rdf2xml.jar ../imx-geo-ontologie.ttl ../imx-geo-model.md libs/rdf2md.xsl
+@REM REM  Seventh step: create MD file for respec page (werkt nog niet)
+@REM REM java -jar libs/rdf2xml.jar ../imx-geo-ontologie.ttl ../imx-geo-model.md libs/rdf2md.xsl
--- mim-ld-export/convert.sh
+++ mim-ld-export/convert.sh
@@ -8,25 +8,25 @@
 #
 
 #Debug
-#java -jar libs/ea2rdf.jar -e "../nen3610-2022-template.eap" > nen3610-2022-db.ttl
+#java -jar mim-ld-export/libs/ea2rdf.jar -e "../nen3610-2022-template.eap" > nen3610-2022-db.ttl
 
 # First step: transform native EAP format to RDF
-java -jar libs/ea2rdf.jar -ea -e "../nen3610-2022-template.eap" > nen3610-2022-ea.ttl
+java -jar mim-ld-export/libs/ea2rdf.jar -ea -0 -e ea/imx-geo.eapx > mim-ld-export/temp/imx-geo-ea.ttl
 
 # Second step: transform EA model in RDF to MIM in RDF
-java -jar libs/rdf2rdf.jar nen3610-2022-ea.ttl nen3610-2022-mim-all.ttl libs/ea2mim.yaml
+java -jar mim-ld-export/libs/rdf2rdf.jar mim-ld-export/temp/imx-geo-ea.ttl mim-ld-export/temp/imx-geo-mim-all.ttl mim-ld-export/libs/wau-ea2mim.yaml
 
 # Third step: get only the NEN3610 Informatimodel from the EA model
-java -jar libs/rdf2rdf.jar nen3610-2022-mim-all.ttl nen3610-2022-mim.ttl nen3610-split.yaml
+java -jar mim-ld-export/libs/rdf2rdf.jar mim-ld-export/temp/imx-geo-mim-all.ttl mim-ld-export/model/imx-geo-mim.ttl mim-ld-export/libs/imx-geo-split.yaml
 
 # Fourth step: transform MIM model in RDF to RDFS/OWL/SHACL ontology
-java -jar libs/rdf2rdf.jar nen3610-2022-mim.ttl nen3610-2022-ont.ttl libs/mim2onto.yaml
+java -jar mim-ld-export/libs/rdf2rdf.jar mim-ld-export/model/imx-geo-mim.ttl mim-ld-export/model/imx-geo-ont.ttl mim-ld-export/libs/wau-mim2onto.yaml
 
 # Fifth step: specific NEN3610 steps
-java -jar libs/rdf2rdf.jar nen3610-2022-ont.ttl ../nen3610-2022-ontologie.ttl nen3610.yaml nen3610-2022-mim.ttl
+# java -jar mim-ld-export/libs/rdf2rdf.jar nen3610-2022-ont.ttl ../nen3610-2022-ontologie.ttl nen3610.yaml nen3610-2022-mim.ttl
 
 # Sixth step: create diagram from ontology
-java -jar libs/rdf2xml.jar ../nen3610-2022-ontologie.ttl nen3610-2022-model.graphml libs/rdf2graphml.xsl
+# java -jar mim-ld-export/libs/rdf2xml.jar ../nen3610-2022-ontologie.ttl nen3610-2022-model.graphml mim-ld-export/libs/rdf2graphml.xsl
 
 # Seventh step: create MD file for respec page
-java -jar libs/rdf2xml.jar ../nen3610-2022-ontologie.ttl ../respec/nen3610-2022-model.md ~/GITREPO/rdf2xml/rdf2md.xsl
+# java -jar mim-ld-export/libs/rdf2xml.jar ../nen3610-2022-ontologie.ttl ../respec/nen3610-2022-model.md ~/GITREPO/rdf2xml/rdf2md.xsl
